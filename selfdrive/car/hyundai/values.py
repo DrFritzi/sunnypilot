ba32240228942280816f0fb80f4518c8ec00b961
Patrick Kolar <pk@nxrt.io>
2022-07-02 11:08:48 +0200

    (dangerous) test max steer for all cars

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -16,7 +16,7 @@ class CarControllerParams:
                              CAR.KIA_K5_2021, CAR.KONA_EV, CAR.KONA, CAR.IONIQ_HEV_2022, CAR.SANTA_FE_HEV_2022]:
       self.STEER_MAX = 384
     else:
-      self.STEER_MAX = 255
+      self.STEER_MAX = 384
     self.STEER_DELTA_UP = 3
     self.STEER_DELTA_DOWN = 7
     self.STEER_DRIVER_ALLOWANCE = 50
