482e706a6a469e623c817f3df6b0670407fd6d4f
Patrick Kolar <pk@nxrt.io>
2022-07-02 13:17:26 +0200

    Revert "(dangerous) test max steer for all cars"
    This reverts commit 0c991853b456bd54a9dfd830caeb9fcfd41cb2bf.

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -17,7 +17,7 @@ class CarControllerParams:
                              CAR.KIA_NIRO_HEV_2021]: #For Kia niro 2020 test
       self.STEER_MAX = 384
     else:
-      self.STEER_MAX = 384
+      self.STEER_MAX = 255
     self.STEER_DELTA_UP = 3
     self.STEER_DELTA_DOWN = 7
     self.STEER_DRIVER_ALLOWANCE = 50
